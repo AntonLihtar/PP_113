f7ecf01b35ed19e3f6c84a39a0f6ac3980526d9b
AntonLihtar <sa-fa@mail.ru>
2022-10-27 12:37:58 +0300

    ok

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -9,103 +9,83 @@ import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
     public UserDaoJDBCImpl() { //default constructor
+    }
+
+    public void createUsersTable() {
+        try (Connection connection = Util.getConnection();
+             Statement stat = connection.createStatement()) {
+            stat.execute("CREATE TABLE IF NOT EXISTS user(id BIGINT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255) NOT NULL, " +
+                    "last_name VARCHAR(255) NOT NULL, age INT NOT NULL )");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
-    public void createUsersTable() throws SQLException { //create table Users
-//        Statement stat = null;
-        Connection connection = Util.getConnection(); //connect
-
-        Statement stat = connection.createStatement(); //CONTAINER
-
-        stat.executeUpdate("CREATE TABLE IF NOT EXISTS user(id BIGINT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255) NOT NULL, " +
-                "last_name VARCHAR(255) NOT NULL, age INT NOT NULL )");
-        System.err.println("-> TABLE CREATE");
-        stat.close();
-        connection.close();
+    public void dropUsersTable() {
+        try (Connection connection = Util.getConnection();
+             Statement stat = connection.createStatement()) {
+            stat.execute("DROP TABLE IF EXISTS user");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
-    public void dropUsersTable() throws SQLException {
-
-        Connection connection = Util.getConnection(); //connect
-
-        Statement stat = connection.createStatement(); //CONTAINER
-
-        stat.executeUpdate("DROP TABLE IF EXISTS user");
-        System.err.println("-> TABLE DROP");
-        stat.close();
-        connection.close();
+    public void saveUser(String name, String lastName, byte age) {
+        try (Connection connection = Util.getConnection();
+             var ppStmt = connection.prepareStatement("INSERT INTO user (name, last_name, age) VALUES(?, ?, ?)")) {
+            ppStmt.setString(1, name); //подставляем символы
+            ppStmt.setString(2, lastName);   //то же
+            ppStmt.setByte(3, age);
+            ppStmt.executeUpdate();
+            System.err.println("User с именем – " + name + " добавлен в базу данных");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
-    public void saveUser(String name, String lastName, byte age) throws SQLException {
+    public void removeUserById(long id) {
+        try (Connection connection = Util.getConnection();
+             PreparedStatement ppStmt = connection.prepareStatement("DELETE FROM user WHERE ID=?")) {
+            ppStmt.setLong(1, id);
+            ppStmt.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
 
-        Connection connection = Util.getConnection(); //connect
-        String sql = "INSERT INTO user (name, last_name, age) VALUES(?, ?, ?)";
-
-        PreparedStatement preparedStatement = connection.prepareStatement(sql); //CONTAINER
-
-        preparedStatement.setString(1, name); //подставляем символы
-        preparedStatement.setString(2, lastName);   //то же
-        preparedStatement.setByte(3, age);
-
-        preparedStatement.executeUpdate();
-        System.err.println("User с именем – " + name + " добавлен в базу данных");
-
-        preparedStatement.close();
-        connection.close();
     }
 
-    public void removeUserById(long id) throws SQLException {
-
-        Connection connection = Util.getConnection(); //connect
-        String sql = "DELETE FROM user WHERE ID=?";
-        PreparedStatement preparedStatement = connection.prepareStatement(sql); //CONTAINER
-
-        preparedStatement.setLong(1, id);
-
-        preparedStatement.executeUpdate();
-
-        System.err.println("-> User delete");
-        preparedStatement.close();
-        connection.close();
-    }
-
-    public List<User> getAllUsers() throws SQLException {
+    public List<User> getAllUsers() {
 
         List<User> userList = new ArrayList<>();
-        Connection connection = Util.getConnection(); //connect
+        try (Connection connection = Util.getConnection();
+             Statement stat = connection.createStatement()) {
 
-        String sql = "SELECT id, name, last_name, age FROM user";
-        Statement stat = connection.createStatement(); //CONTAINER
-
-        ResultSet resultSet = stat.executeQuery(sql);
-        System.err.print("-> Start while ");
-        while (resultSet.next()) {
-            User user = new User();
-            user.setId(resultSet.getLong("id"));
-            user.setName(resultSet.getString("name"));
-            user.setLastName(resultSet.getString("last_name"));
-            user.setAge(resultSet.getByte("age"));
-            userList.add(user);
+            ResultSet resultSet = stat.executeQuery("SELECT id, name, last_name, age FROM user");
+            System.err.print("-> Start while ");
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getLong("id"));
+                user.setName(resultSet.getString("name"));
+                user.setLastName(resultSet.getString("last_name"));
+                user.setAge(resultSet.getByte("age"));
+                userList.add(user);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
-        System.err.println("-> Good like");
-        stat.close();
-        connection.close();
         return userList;
     }
 
-    public void cleanUsersTable() throws SQLException {
+    public void cleanUsersTable() {
 
-        Connection connection = Util.getConnection(); //connect
-
-        Statement stat = connection.createStatement(); //CONTAINER
-
-        stat.executeUpdate("DELETE FROM user");
-
-        System.err.println("-> Users table cleared");
-        stat.close();
-        connection.close();
+        try(Connection connection = Util.getConnection();
+        Statement stat = connection.createStatement()) {
+            stat.execute("TRUNCATE user");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,19 +1,18 @@
 package jm.task.core.jdbc.util;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class Util throws SQLException{
-   
-    private static final String DB_URL = "jdbc:mysql://localhost:3306/db113";
+public class Util {
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/db_hib";
     private static final String DB_USER = "root";
-    private static final String DB_PASS = "anton";
-
-
+    private static final String DB_PASS = "root";
     public static Connection getConnection() {
-        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASS)) {
-            System.out.println("Connection OK");
-            return connection;
+        try {
+            return DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
         }
     }
 }
